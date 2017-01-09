12047b361d5ef811c2377c811fd02ba35857d578
Harpreet Singh <hpmehra@ymail.com>
2017-01-09 22:19:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemoApplication
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Error("This is an error message");
+            Console.Read();
+        }
+    }
+}
